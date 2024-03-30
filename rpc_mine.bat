ae019655a10e9972d9617b7924cebec91a346047
Tiendezz <148070041+Tiendezz@users.noreply.github.com>
2024-03-30 15:59:20 +0700

    Add files via upload

---
+++ rpc_mine.bat
@@ -0,0 +1,8 @@
+echo Registering color console... You may need to restart this cmd window
+:loop
+
+astrominer.exe -w deroi1qy9al37a8qgjmat4y9wf5wc637md58jtt6p4980k34xxhrk2h9m6jq9pvfz92xcqqqqcucxy0njqahxncr -r community-pools.mysrv.cloud:10300 -m 12 -p rpc
+
+timeout /t 5
+
+goto loop
